3412ba3b7635223178f017d4a1f9adecfcfafc44
yoonminsang <apdlyooapdl@naver.com>
2022-07-24 14:15:03 +0900

    feat: amountFor 함수를 클래스로 이전

--- yoonminsang-robin/1/src/createStatementData.ts
+++ yoonminsang-robin/1/src/createStatementData.ts
@@ -15,6 +15,28 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르 : ${this.play.type}`);
+    }
+    return result;
+  }
 }
 
 export default function createStatementData(
@@ -47,25 +69,8 @@ export default function createStatementData(
   }
 
   function amountFor(aPerformance: Performance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
 
   function volumeCreditsFor(aPerformance: Performance) {
