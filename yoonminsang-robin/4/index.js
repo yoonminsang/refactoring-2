a7deadc2891be108705f5201595a1585d7630507
yoonminsnag <apdlyooapdl@naver.com>
2022-08-28 01:02:55 +0900

    feat: 수익을 계산하는 로직 추가

--- yoonminsang-robin/4/index.js
+++ yoonminsang-robin/4/index.js
@@ -38,6 +38,27 @@ class Province {
   get shortfall() {
     return this._demand - this.totalProduction;
   }
+  get profit() {
+    return this.demandValue - this.demandCost;
+  }
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+  get demandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((p) => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
+    return result;
+  }
 }
 
 function sampleProvinceData() {
